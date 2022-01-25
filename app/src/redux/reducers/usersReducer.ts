6f5b02f5db70a2b6dc0ba43af0ad9177892a7b6e
Sudip Raj Kunwar <sudip.kunwar9898@gmail.com>
2022-01-25 10:34:51 +0545

    Axios erro fix

--- app/src/redux/reducers/usersReducer.ts
+++ app/src/redux/reducers/usersReducer.ts
@@ -16,6 +16,7 @@ export const usersReducer = (state = initialState, action: AnyAction) => {
         ...state,
         isLoading: true,
       };
+
     case types.SIGNIN_USER_SUCCESS:
     case types.SIGNUP_USER_SUCCESS:
       return {
@@ -24,24 +25,28 @@ export const usersReducer = (state = initialState, action: AnyAction) => {
         error: null,
         data: action.payload,
       };
+
     case types.GET_USER_DETAIL_SUCCESS:
       return {
         ...state,
         isLoading: false,
         data: action.payload,
       };
+
     case types.USER_REQUEST_FAILED:
       return {
         ...state,
         error: action.error,
         isLoading: false,
       };
+
     case types.SIGNOUT_USER:
       return {
         ...state,
         error: null,
         isLoading: false,
       };
+
     default:
       return { ...state };
   }
